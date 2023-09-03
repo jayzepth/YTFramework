0c79d80e357a6e4d5184e1eeff75a15e7b663002
Admin <Admin@DESKTOP-BOIP2TG>
2023-09-03 14:12:06 +0530

    YT Second Push

--- YTFramework/src/test/java/org/testing/TestScript/TC1.java
+++ YTFramework/src/test/java/org/testing/TestScript/TC1.java
@@ -18,23 +18,23 @@ public class TC1 extends Base
 	@Test
 	public void Trending() throws InterruptedException, IOException
 	{
-		ExtentReports report = ReportHandling.TakeReport("C:\\\\Users\\\\Jay\\\\Desktop\\\\ScreenShot\\\\Report.html");
+		ExtentReports report = ReportHandling.TakeReport("C:\\\\Users\\\\Admin\\\\Desktop\\\\ScreenShot\\\\Report.html");
 		ExtentTest test = report.startTest("TC1");
 		
 		LoginPage Login = new LoginPage(driver, pr);
 		Login.Signin("jayautomation2@gmail.com", "Manu@5252");
 		
-		ScreenShotCapture.takeScreenShot(driver, "C:\\Users\\Jay\\Desktop\\ScreenShot\\HomePage.jpg");
+		ScreenShotCapture.takeScreenShot(driver, "C:\\Users\\Admin\\Desktop\\ScreenShot\\HomePage.jpg");
 		
 		Assert.Assert1("https://www.youtube.com/", driver.getCurrentUrl(), test);
 		HomePage homepage = new HomePage(driver, pr);
 		homepage.NavigateToTranding();
-		ScreenShotCapture.takeScreenShot(driver, "C:\\Users\\Jay\\Desktop\\ScreenShot\\TrandingPage.jpg");
+		ScreenShotCapture.takeScreenShot(driver, "C:\\Users\\Admin\\Desktop\\ScreenShot\\TrandingPage.jpg");
 		Assert.Assert1("https://www.youtube.com/", driver.getCurrentUrl(), test);
 		
 		LogoutPage Logout = new LogoutPage(driver, pr);
 		Logout.SignOut();
-		ScreenShotCapture.takeScreenShot(driver, "C:\\Users\\Jay\\Desktop\\ScreenShot\\Signout.jpg");
+		ScreenShotCapture.takeScreenShot(driver, "C:\\Users\\Admin\\Desktop\\ScreenShot\\Signout.jpg");
 		Assert.Assert1("https://www.youtube.com/", driver.getCurrentUrl(), test);
 		
 		report.endTest(test);
